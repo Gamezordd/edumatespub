c1170912bc6e19e888d9feb68a1f476533a4acbf
Soumik Majumder <soumik712@gmail.com>
2020-06-21 18:39:09 +0530

    Unresolved conflict on remote

--- src/components/navigation/NavWrapper.tsx
+++ src/components/navigation/NavWrapper.tsx
@@ -13,11 +13,7 @@ const mapStateToProps = (state: any) => {
 };
 
 const userLeftItems: ItemProps[] = [
-<<<<<<< HEAD
-	{ content: 'Home', key: 'home', to: '/'},
-=======
 	{ content: 'Home', key: 'home', to: '/home' },
->>>>>>> 558a10bb386d9f2bc5069d0fbbc762d64b52adf8
 	{ content: 'Maps', key: 'maps', to: '/maps' },
 	{ content: 'Chat', key: 'chat', to: '/chat' },
 	{ content: 'Discover', key: 'discover', to: '/discover' },
@@ -29,7 +25,7 @@ const userRightItems: ItemProps[] = [
 
 const rightItems: ItemProps[] = [
 	{ content: 'Register', key: 'register', to: '/register' },
-	{ content: 'Login', key: 'login', to: '/login'},
+	{ content: 'Login', key: 'login', to: '/login' },
 ];
 
 const NavBarWrapperUncomposed: React.FC<any> = ({
